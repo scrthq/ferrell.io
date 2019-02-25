2470bfa7d9845253453ba60426dc1c6411d09d4f
Nate Ferrell <nate@scrthq.com>
2019-02-24 22:55:52 -0600

    added ascii art to darkmode.js

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,38 +1,29 @@
-/*!
- *                                                                ..       ..           .
- *   oec :                                                  x .d88"  x .d88"           @88>
- *  @88888                .u    .      .u    .               5888R    5888R            %8P          u.
- *  8"*88%       .u     .d88B :@8c   .d88B :@8c       .u     '888R    '888R             .     ...ue888b
- *  8b.       ud8888.  ="8888f8888r ="8888f8888r   ud8888.    888R     888R           .@88u   888R Y888r
- * u888888> :888'8888.   4888>'88"    4888>'88"  :888'8888.   888R     888R          ''888E`  888R I888>
- *  8888R   d888 '88%"   4888> '      4888> '    d888 '88%"   888R     888R            888E   888R I888>
- *  8888P   8888.+"      4888>        4888>      8888.+"      888R     888R            888E   888R I888>
- *  *888>   8888L       .d888L .+    .d888L .+   8888L        888R     888R      .     888E  u8888cJ888
- *  4888    '8888c. .+  ^"8888*"     ^"8888*"    '8888c. .+  .888B .  .888B .  .@8c    888&   "*888*P"
- *  '888     "88888%       "Y"          "Y"       "88888%    ^*888%   ^*888%  '%888"   R888"    'Y"
- *   88R       "YP'                                 "YP'       "%       "%      ^*      ""
- *   88>
- *   48
- *   '8
- *
- * Dark Mode theme toggle activated!
- */
-
 console.log("                                                               ..       ..           .\r\n  oec :                                                  x .d88'  x .d88'           @88>\r\n @88888                .u    .      .u    .               5888R    5888R            %8P          u.\r\n 8'*88%       .u     .d88B :@8c   .d88B :@8c       .u     '888R    '888R             .     ...ue888b\r\n 8b.       ud8888.  ='8888f8888r ='8888f8888r   ud8888.    888R     888R           .@88u   888R Y888r\r\nu888888> :888'8888.   4888>'88'    4888>'88'  :888'8888.   888R     888R          ''888E`  888R I888>\r\n 8888R   d888 '88%'   4888> '      4888> '    d888 '88%'   888R     888R            888E   888R I888>\r\n 8888P   8888.+'      4888>        4888>      8888.+'      888R     888R            888E   888R I888>\r\n *888>   8888L       .d888L .+    .d888L .+   8888L        888R     888R      .     888E  u8888cJ888\r\n 4888    '8888c. .+  ^'8888*'     ^'8888*'    '8888c. .+  .888B .  .888B .  .@8c    888&   '*888*P'\r\n '888     '88888%       'Y'          'Y'       '88888%    ^*888%   ^*888%  '%888'   R888'    'Y'\r\n  88R       'YP'                                 'YP'       '%       '%      ^*      ''\r\n  88>\r\n  48\r\n  '8\n\n");
 console.log("Dark Mode theme toggle online!");
 
 if (getCookie("theme") == "dark") {
     $('html').addClass("dark");
+} else if (getCookie("theme") == "code") {
+    $('html').addClass("code");
+} else {
+    $('html').addClass("light");
 }
 
 $(document).ready(function() {
     $(".theme-toggle").click(function(e) {
         e.preventDefault();
-        $('html').toggleClass("dark");
         if ($('html').hasClass("dark")) {
+            $('html').toggleClass("dark",null,false);
+            $('html').toggleClass("light",null,true);
+            document.cookie = "theme=light;path=/";
+        } else if ($('html').hasClass("code")) {
+            $('html').toggleClass("code",null,false);
+            $('html').toggleClass("dark",null,true);
             document.cookie = "theme=dark;path=/";
         } else {
-            document.cookie = "theme=light;path=/";
+            $('html').toggleClass("light",null,false);
+            $('html').toggleClass("code",null,true);
+            document.cookie = "theme=code;path=/";
         }
     });
 });
